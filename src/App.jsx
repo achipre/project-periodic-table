3967d2710c7d95d15ffbe47490a1640c7ed9daba
Alex Chipre <alechipre@gmail.com>
2023-10-24 19:44:14 -0500

    Add Grid template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import datos from './mock/elements.json'
 import './App.css'
+// import { useEffect } from 'react'
 export const App = () => {
+  // useEffect(() => {
+  //   const datafetch = async datos
+  //   const res = await res.json()
+  //   const data = await data
+  // .then(res => res.json())
+  // .then(data => console.log(data))
+  // }, [])
   return (
-    <h1>
-      {datos.forEach(dato => <h1>Hola</h1>)}
-    </h1>
+    <main className="tab-periodic">
+      {datos.elements.map(dato => (
+        <article className={dato.name} key={dato.name}>
+          <p>{dato.number}</p>
+          <p>{dato.symbol}</p>
+          <p>{dato.name}</p>
+        </article>
+      ))}
+    </main>
   )
 }
